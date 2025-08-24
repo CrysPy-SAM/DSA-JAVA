c618563f3d3bc14d5ff6ac697622dc76470d59e0
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-24 12:36:17 +0530

    Add Product, Sum, and VarAndDataType classes with basic arithmetic operations and output

---
+++ java_basics/VarAndDataType.java
@@ -0,0 +1,12 @@
+public class VarAndDataType {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.print("Hello World!\n");
+        System.out.print("Hello World!\n");
+        System.out.print("Hello World!\n");
+
+        
+    }
+}
