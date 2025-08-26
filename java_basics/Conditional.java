8389abc7a42b0bf882fb24962f29eaa03d40d056
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-26 11:05:27 +0530

    Comment out even/odd number check and else-if age classification examples for clarity

--- java_basics/Conditional.java
+++ java_basics/Conditional.java
@@ -24,14 +24,33 @@ public class Conditional {
 
         //print an number is even or odd
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int num = sc.nextInt();
-        if (num % 2 == 0) {
-            System.out.println("Even number");
-        } else {
-            System.out.println("Odd number");
-        }
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the number");
+        // int num = sc.nextInt();
+        // if (num % 2 == 0) {
+        //     System.out.println("Even number");
+        // } else {
+        //     System.out.println("Odd number");
+        // }
+
+
+        //else-if
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the number");
+        // int age= sc.nextInt();
+        // if (age>=18) {
+        //     System.out.println("Adult");
+        // }
+        // else if(age>=13  && age<=18){
+        //       System.out.println("teeneger");
+        // }
+        // else if(age >= 1  &&  age < 13){
+        //     System.out.println("Children");
+        // }
+
+
+
+        
     }
 
 }
