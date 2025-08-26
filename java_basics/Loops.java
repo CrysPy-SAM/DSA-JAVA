4c98e2fc76bab5c29c940d5369403e7bee490b42
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-26 12:43:37 +0530

    Comment out unused code sections for clarity

---
+++ java_basics/Loops.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Loops {
+    public static void main(String[] args) {
+    //     int counter = 0;
+    //     while (counter < 100) {
+    //         System.out.println("Hello World!");
+    //         counter++;
+    //     }
+
+    //     System.out.println("Printed Hellow Worlds! 100s times");
+    
+
+    //Print num 1 to 10
+
+    // int counter = 1;
+    // while(counter <= 10){
+    //     System.out.println(counter);
+    //     counter++;
+    // }
+
+    //print number from 1 to n
+     
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter Num");
+    int range = sc.nextInt();
+    int counter = 1;
+
+    while (counter <= range) {
+        System.out.print(counter +" ");
+        counter++;
+    }
+    System.out.println();
+}
+}
