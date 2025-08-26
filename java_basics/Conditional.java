2e84903c20e6c363779f09e6b8752e7d317ba580
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-26 10:53:40 +0530

    Add Conditional class with main method for basic conditional statements

---
+++ java_basics/Conditional.java
@@ -0,0 +1,23 @@
+public class Conditional {
+    public static void main(String[] args) {
+        // int age = 17;
+        // if(age>=18){
+        //       System.out.println("eligible for the drive || Voting");
+        // }
+        // else{
+        //     System.out.println("Not eligible for drive || Voting");
+        // }
+
+        //Finding largest Num
+
+        int a=1;
+        int b=3;
+
+        if(a>=b){
+             System.out.println("a is greter than b");
+        }
+        else{
+            System.out.println("b is greter than a");
+        }
+    }
+}
