12c78d66ed4775417d4c1ede4490fa1710c8d982
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-28 10:05:25 +0530

    Refactor star pattern logic to use dynamic size and clean up commented code

--- PATTERNS/star.java
+++ PATTERNS/star.java
@@ -2,12 +2,24 @@ package PATTERNS;
 import java.util.*;
 
 public class star {
+    // public static void main(String[] args) {
+    //     for (int  line =1; line<=4; line++){
+    //         for (int star=1; star<=line; star++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    //INVERTED
     public static void main(String[] args) {
-        for (int  line =1; line<=4; line++){
-            for (int star=1; star<=line; star++){
+        int n=4;
+        for(int line=1; line<=n; line++){
+            for(int star=1; star<=n-line+1; star++){
                 System.out.print("*");
             }
             System.out.println();
+
         }
     }
     
