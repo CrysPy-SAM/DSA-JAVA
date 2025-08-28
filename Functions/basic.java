b7c6ce59726a9a8e2e9d8c2dfaeabe53d1c64e22
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-28 16:54:12 +0530

    Refactor basic class to implement binomial coefficient calculation and update main method to test prime number functionality

--- Functions/basic.java
+++ Functions/basic.java
@@ -36,7 +36,26 @@ public class basic {
         }
         return f;
     }
+
+    public static int bionoCo(int n, int r){
+                 int fact_n= factorial(n);
+                 int fact_r= factorial(r);
+                 int fact_nmr= factorial(n-r);
+                 
+                 int  bionoCo = fact_n/ (fact_r*fact_nmr);
+                 return bionoCo;
+    }
+
+    public static boolean isPrime(int n){
+        boolean isPrime = true;
+        for(int i=2; i<=n-1; i++){
+            if(n%i==0){
+                isPrime=false;
+            }
+        }
+        return isPrime;
+    }
     public static void main(String[] args) {
-        System.out.println(factorial(4));
+       System.out.println(isPrime(5));
     }
 }
