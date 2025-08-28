e9986f13d23bc13cb31875c8cd5511723fc31e37
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-28 11:57:51 +0530

    Add basic class with printHello method to display "Hello World!"

---
+++ Functions/basic.java
@@ -0,0 +1,13 @@
+package Functions;
+
+public class basic {
+    public static void printHello(){
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+}
