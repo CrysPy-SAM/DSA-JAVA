a908b342c6c046fd00bd555fdf8da0f449922f6b
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-28 12:08:28 +0530

    Refactor basic class to remove duplicate printHello calls and implement user input for sum calculation

--- Functions/basic.java
+++ Functions/basic.java
@@ -1,5 +1,7 @@
 package Functions;
 
+import java.util.Scanner;
+
 public class basic {
     public static void printHello(){
         System.out.println("Hello World!");
@@ -7,7 +9,17 @@ public class basic {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
     }
+
+    public static void CalculateSum(int num1, int num2){
+        int sum = num1+num2;
+        System.out.println("sum is: " +sum);
+    }
     public static void main(String[] args) {
-        printHello();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st num");
+        int a = sc.nextInt();
+        System.out.println("Enter 2nd num");
+        int b=sc.nextInt();
+        CalculateSum(a, b);
     }
 }
