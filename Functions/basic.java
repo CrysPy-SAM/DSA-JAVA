8349cd3ee1f236614ab076011f906cc6598d3ed6
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-29 15:17:55 +0530

    Clean up commented-out code and improve isPrime method for better readability

--- Functions/basic.java
+++ Functions/basic.java
@@ -27,35 +27,49 @@ public class basic {
     //Factorial
 
 
-    public static int factorial(int n){
+    // public static int factorial(int n){
 
-        int f=1;
+    //     int f=1;
 
-        for(int i=1; i<=n; i++){
-            f=f*i;
-        }
-        return f;
-    }
+    //     for(int i=1; i<=n; i++){
+    //         f=f*i;
+    //     }
+    //     return f;
+    // }
 
-    public static int bionoCo(int n, int r){
-                 int fact_n= factorial(n);
-                 int fact_r= factorial(r);
-                 int fact_nmr= factorial(n-r);
+    // public static int bionoCo(int n, int r){
+    //              int fact_n= factorial(n);
+    //              int fact_r= factorial(r);
+    //              int fact_nmr= factorial(n-r);
                  
-                 int  bionoCo = fact_n/ (fact_r*fact_nmr);
-                 return bionoCo;
-    }
+    //              int  bionoCo = fact_n/ (fact_r*fact_nmr);
+    //              return bionoCo;
+    // }
+
+    // public static boolean isPrime(int n){
+    //     boolean isPrime = true;
+    //     for(int i=2; i<=n-1; i++){
+    //         if(n%i==0){
+    //             isPrime=false;
+    //         }
+    //     }
+    //     return isPrime;
+    // }
+
 
     public static boolean isPrime(int n){
-        boolean isPrime = true;
-        for(int i=2; i<=n-1; i++){
-            if(n%i==0){
-                isPrime=false;
-            }
+        if(n ==2){
+            return true;
         }
-        return isPrime;
+       for (int i =2; i<=Math.sqrt(n); i++){
+        if (n%i == 0){
+            return false;
+        }
+       
+       }
+        return true;
     }
     public static void main(String[] args) {
-       System.out.println(isPrime(5));
+       System.out.println(isPrime(7));
     }
 }
