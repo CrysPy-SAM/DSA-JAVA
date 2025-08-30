26be5a0fbfda7bbe891dc6fad7c59b4b468af9c6
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-30 16:19:16 +0530

    Fix indentation and clean up main method by commenting out hollow rectangle call

--- adv_Patterns/adv_Patterns.java
+++ adv_Patterns/adv_Patterns.java
@@ -11,7 +11,22 @@ public class adv_Patterns{
             System.out.println();
         }
     }
+
+
+
+    public static void inverted_roteted_half_pyramid(int n){
+        for (int  i =1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        hollow_ractangle(4, 5);
+        // hollow_ractangle(4, 5);
+        inverted_roteted_half_pyramid(4);
     }
 }
