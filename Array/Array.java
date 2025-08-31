44f8963221404b42dfa4699e9cb2424848b98a46
CrysPy-SAM <ssmishra9918@gmail.com>
2025-09-01 00:14:59 +0530

    Add initial Array class implementation with main method and sample arrays

---
+++ Array/Array.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class Array {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+        int number[] = {1,2,3};
+        int moreNumbers[] = {4,5,6};
+        String fruits[] = {"apple", "mango", "gvava"};
+    }
+}
