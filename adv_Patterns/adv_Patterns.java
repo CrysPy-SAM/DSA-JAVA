377d60c22f7edbb28c539204773bbb0b00671c46
CrysPy-SAM <ssmishra9918@gmail.com>
2025-08-31 12:41:05 +0530

    Refactor dimond method for clarity and update main method to call dimond instead of floyes_trangle

--- adv_Patterns/adv_Patterns.java
+++ adv_Patterns/adv_Patterns.java
@@ -38,9 +38,39 @@ public class adv_Patterns{
              
           }
     }
+
+    public static  void  dimond(int n){
+        //  1st half
+
+        for (int i=1; i<=n; i++){
+            //spaces
+            for (int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            //stars
+            for( int j=1; j<=(2*i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2nd half
+        for (int i=n; i>=1; i--){
+            //spaces
+            for (int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            //stars
+            for( int j=1; j<=(2*i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // hollow_ractangle(4, 5);
         // inverted_roteted_half_pyramid(4);
-        floyes_trangle(5);
+        // floyes_trangle(5);
+        dimond(4);
     }
 }
