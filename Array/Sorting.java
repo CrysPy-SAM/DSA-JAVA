a671f9d70a7c4037aff37408555dfd92ac5674b1
CrysPy-SAM <ssmishra9918@gmail.com>
2025-09-06 11:24:53 +0530

    insertion sort

--- Array/Sorting.java
+++ Array/Sorting.java
@@ -33,9 +33,24 @@ class Solution {
         }
     }
 
+    public static void InsertiponSort(int arr[]) {
+        for(int i=1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev =i-1;
+
+            //finding out the corrct pos to insert 
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1] =arr[prev];
+                prev--;
+            }
+            //Insertion
+            arr[prev+1] = curr;
+         }
+    }
+
     public static void main(String args[]){
         int arr[] = {5, 4, 1, 3, 2};
-        selectionSort(arr);   
+        InsertiponSort(arr);   
         printArr(arr);  
     }
 }
