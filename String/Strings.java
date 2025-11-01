fabc9c3562ffbfa77595ddfce891dd6710273087
CrysPy-SAM <ssmishra9918@gmail.com>
2025-11-01 11:06:25 +0530

    STRINGS

---
+++ String/Strings.java
@@ -0,0 +1,12 @@
+package String;
+
+public class Strings {
+    public static void main(String[] args) {
+        char arr[] = {'h', 'e', 'l', 'l', 'o'};
+        String str = "abcde";
+        String str2 = new String("xyz");
+        String str3 = new String(arr);
+
+        //STRINGS ARE IMMUTABLE (NEVER CHANGEBLE)
+    }
+}
