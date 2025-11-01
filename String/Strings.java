12c253bf24954615e7a493119d790cc862d282c6
CrysPy-SAM <ssmishra9918@gmail.com>
2025-11-01 11:25:39 +0530

    .lenth()

--- String/Strings.java
+++ String/Strings.java
@@ -1,20 +1,23 @@
-
 package String;
 
 import java.util.Scanner;
 
 public class Strings {
     public static void main(String[] args) {
-        char arr[] = {'h', 'e', 'l', 'l', 'o'};
-        String str = "abcde";
-        String str2 = new String("xyz");
-        String str3 = new String(arr);
+        // char arr[] = {'h', 'e', 'l', 'l', 'o'};
+        // String str = "abcde";
+        // String str2 = new String("xyz");
+        // String str3 = new String(arr);
 
-        // STRINGS ARE IMMUTABLE (NEVER CHANGEABLE)
+        // // STRINGS ARE IMMUTABLE (NEVER CHANGEABLE)
 
-        Scanner sc = new Scanner(System.in);
-        String name; 
-        name = sc.nextLine(); // Input with spaces 
-        System.out.println(name);
+        // Scanner sc = new Scanner(System.in);
+        // String name; 
+        // name = sc.nextLine(); // Input with spaces 
+        // System.out.println(name);
+
+        String fullName = "John Doe";
+        System.out.println(fullName);          // prints the full name
+        System.out.println(fullName.length()); // prints the length of the string
     }
 }
