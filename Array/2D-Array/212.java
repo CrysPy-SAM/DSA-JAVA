ffe467d93067ea9d007c34cd1ba76ff8309ef855
CrysPy-SAM <ssmishra9918@gmail.com>
2025-11-03 22:38:06 +0530

    212

---
+++ Array/2D-Array/212.java
@@ -0,0 +1,40 @@
+class Solution {
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        String word;
+    }
+
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String w : words) {
+            TrieNode node = root;
+            for (char c : w.toCharArray()) {
+                int idx = c - 'a';
+                if (node.children[idx] == null) node.children[idx] = new TrieNode();
+                node = node.children[idx];
+            }
+            node.word = w;
+        }
+        return root;
+    }
+
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        int m = board.length, n = board[0].length;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                dfs(board, i, j, root, res);
+        return res;
+    }
+
+    private void dfs(char[][] board, int i, int j, TrieNode node, List<String> res) {
+        char c = board[i][j];
+        if (c == '#' || node.children[c - 'a'] == null) return;
+        node = node.children[c - 'a'];
+        if (node.word != null) {
+            res.add(node.word);
+            node.word = null;
+        }
+      
+}
