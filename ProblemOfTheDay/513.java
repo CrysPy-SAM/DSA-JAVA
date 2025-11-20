d2d7eaa22664a2b5ac69675bb96fcde744e19126
CrysPy-SAM <ssmishra9918@gmail.com>
2025-11-20 13:34:38 +0530

    Initial commit: added solution and test cases

--- ProblemOfTheDay/513.java
+++ ProblemOfTheDay/513.java
@@ -1,5 +1,16 @@
-package ProblemOfTheDay;
-
-public class 513 {
-    
+class Solution {
+    public int numSub(String s) {
+        long ans = 0, count = 0;
+        int mod = 1000000007;
+        for (char c : s.toCharArray()) {
+            if (c == '1') {
+                count++;
+            } else {
+                ans = (ans + count * (count + 1) / 2) % mod;
+                count = 0;
+            }
+        }
+        ans = (ans + count * (count + 1) / 2) % mod;
+        return (int) ans;
+    }
 }
