0e131821d9c40b21448691aac56000f904c1f9d8
CrysPy-SAM <ssmishra9918@gmail.com>
2026-02-17 22:44:48 +0530

    string

--- String/02.java
+++ String/02.java
@@ -33,7 +33,7 @@ public class Strings {
    
     public static void printLetters(String str) {
         for (int i = 0; i < str.length(); i++) {
-            System.out.print(str.charAt(i) + " ");  // prints each character with a space
+            System.out.print(str.charAt(i) + " ");
         }
         System.out.println(); // new line at the end
     }
